d405a38b606a9463b51c5c23646a766e7211030c
hamzaavvan <hamzaavvan@gmail.com>
2019-07-05 00:51:25 +0500

    Setting things up
    Added Readme

---
+++ Readme.md
@@ -0,0 +1,10 @@
+# MyCollect App @Ionic
+MyCollect app on <img src="http://blog.pdsullivan.com/assets/ionic-logo.png" width="50" height="18">
+
+1. Ionic
+2. Firebase Database
+
+## What's new
+Manage all of your committees in one place
+
+**Note:** Developement is in progress
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
@@ -35,5 +36,35 @@ import { FirebaseConfig } from './credentials';
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
+=======
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+>>>>>>> b03b26f... Initial commit
 })
 export class AppModule {}
